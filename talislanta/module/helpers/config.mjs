959ff8e15f92411564018f4ad0e4602e3609123c
Vectrum47 <59459215+Vectrum47@users.noreply.github.com>
2022-07-26 15:00:19 -0400

    Addition of base FTT files
    Adding the base system files for a foundry VTT system, after renaming all boilerplate to talislanta to make it workable for our system

---
+++ talislanta/module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const TALISLANTA = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ TALISLANTA.abilities = {
+  "str": "TALISLANTA.AbilityStr",
+  "dex": "TALISLANTA.AbilityDex",
+  "con": "TALISLANTA.AbilityCon",
+  "int": "TALISLANTA.AbilityInt",
+  "wis": "TALISLANTA.AbilityWis",
+  "cha": "TALISLANTA.AbilityCha"
+};
+
+TALISLANTA.abilityAbbreviations = {
+  "str": "TALISLANTA.AbilityStrAbbr",
+  "dex": "TALISLANTA.AbilityDexAbbr",
+  "con": "TALISLANTA.AbilityConAbbr",
+  "int": "TALISLANTA.AbilityIntAbbr",
+  "wis": "TALISLANTA.AbilityWisAbbr",
+  "cha": "TALISLANTA.AbilityChaAbbr"
+};
